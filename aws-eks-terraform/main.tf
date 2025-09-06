c1c550672a7605393106b5d55d1ebbf56395b497
pengchao <pengchao.ma2@outlook.com>
2025-09-06 21:17:35 +0800

    udpated

--- aws-eks-terraform/main.tf
+++ aws-eks-terraform/main.tf
@@ -1,19 +1,23 @@
 data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
 }
-
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
   }
 }
 
