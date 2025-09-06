de3c260de7d7b63453cd211ba711d072bb6f39ab
pengchao <pengchao.ma2@outlook.com>
2025-09-06 16:37:52 +0800

    updated

--- aws-eks-terraform/outputs.tf
+++ aws-eks-terraform/outputs.tf
@@ -1,3 +1,4 @@
+# outputs.tf
 output "cluster_id" {
   description = "EKS cluster ID"
   value       = aws_eks_cluster.main.id
@@ -10,45 +11,42 @@ output "cluster_endpoint" {
 
 output "ubuntu_ami_id" {
   description = "Ubuntu AMI ID used for nodes"
-  value       = data.aws_ami.ubuntu_eks.id
+  value       = data.aws_ami.eks_optimized.id  # 修改为正确的数据源名称
 }
 
-# 修正：使用 splat 表达式输出所有节点组的 ARN
 output "node_group_arns" {
   description = "List of all node group ARNs"
-  value       = aws_eks_node_group.ubuntu_nodes[*].arn
+  value       = aws_eks_node_group.nodes[*].arn  # 修改为正确的资源名称
 }
 
-# 或者如果您只想输出第一个节点组的 ARN
 output "first_node_group_arn" {
   description = "ARN of the first node group"
-  value       = aws_eks_node_group.ubuntu_nodes[0].arn
+  value       = length(aws_eks_node_group.nodes) > 0 ? aws_eks_node_group.nodes[0].arn : null  # 修改为正确的资源名称
 }
 
 output "launch_template_id" {
-  description = "Launch template ID for Ubuntu nodes"
-  value       = aws_launch_template.ubuntu_eks.id
+  description = "Launch template ID for EKS nodes"
+  value       = aws_launch_template.eks_optimized.id  # 修改为正确的资源名称
 }
 
-# 添加更多有用的输出
 output "node_group_names" {
   description = "List of all node group names"
-  value       = aws_eks_node_group.ubuntu_nodes[*].node_group_name
+  value       = aws_eks_node_group.nodes[*].node_group_name  # 修改为正确的资源名称
+}
+
+output "cluster_certificate_authority_data" {
+  description = "EKS cluster certificate authority data"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
+}
+
+output "cluster_status" {
+  description = "EKS cluster status"
+  value       = aws_eks_cluster.main.status
 }
 
 output "cluster_name" {
   description = "EKS cluster name"
   value       = var.cluster_name
-}
-
-output "cluster_version" {
-  description = "EKS cluster version"
-  value       = aws_eks_cluster.main.version
-}
-
-output "cluster_security_group_id" {
-  description = "Security group ID attached to the cluster"
-  value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
 }
 
 output "cluster_iam_role_arn" {
@@ -59,4 +57,9 @@ output "cluster_iam_role_arn" {
 output "node_iam_role_arn" {
   description = "IAM role ARN for the nodes"
   value       = aws_iam_role.node.arn
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the cluster"
+  value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
 }
